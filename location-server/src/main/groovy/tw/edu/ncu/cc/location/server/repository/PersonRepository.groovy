0f0622e9442e5c5259d1b1cff72f1fe4f1f7aa21
jasonChiu <jason1122g@gmail.com>
2015-04-17 23:24:54 +0800

    add repositories

---
+++ location-server/src/main/groovy/tw/edu/ncu/cc/location/server/repository/PersonRepository.groovy
@@ -0,0 +1,11 @@
+package tw.edu.ncu.cc.location.server.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import tw.edu.ncu.cc.location.server.entity.PersonEntity
+
+
+interface PersonRepository extends JpaRepository< PersonEntity, Integer > {
+
+    List< PersonEntity > findByChineseName( String cname )
+
+}
