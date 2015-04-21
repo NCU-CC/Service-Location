7211ad1ac1b2908a76e2ff892daf15d5fef536ce
jasonChiu <jason1122g@gmail.com>
2015-04-21 11:43:53 +0800

    add api token guard

--- location-server/src/main/groovy/tw/edu/ncu/cc/location/server/config/SecurityConfig.groovy
+++ location-server/src/main/groovy/tw/edu/ncu/cc/location/server/config/SecurityConfig.groovy
@@ -1,22 +1,27 @@
 package tw.edu.ncu.cc.location.server.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import tw.edu.ncu.cc.oauth.resource.filter.TokenAccessDecisionFilter
 
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Configuration
     public static class OauthGuard extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        def TokenAccessDecisionFilter tokenAccessDecisionFilter
+
         @Override
         protected void configure( HttpSecurity http ) throws Exception {
-//            http.antMatcher( "/management/**" )
-//                .authorizeRequests()
-//                    .antMatchers( "/management/v1/**" ).access( "hasIpAddress('127.0.0.1') or hasIpAddress('0:0:0:0:0:0:0:1')" )
-//                    .and()
-//                .csrf().disable()
+            http.antMatcher( "/v*/**" )
+                    .addFilterAfter( tokenAccessDecisionFilter, UsernamePasswordAuthenticationFilter.class )
+                    .csrf().disable()
         }
     }
 
