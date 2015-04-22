2b9a5a34142f3e2ea2edc2ecca1adcf802c9892e
jasonChiu <jason1122g@gmail.com>
2015-04-22 10:48:21 +0800

    refactor word service

--- location-data/src/main/groovy/tw/edu/ncu/cc/location/data/keyword/Word.groovy
+++ location-data/src/main/groovy/tw/edu/ncu/cc/location/data/keyword/Word.groovy
@@ -2,6 +2,7 @@ package tw.edu.ncu.cc.location.data.keyword;
 
 public class Word {
 
+    def String index;
     def String word;
     def WordType type;
 
--- location-server/src/test/groovy/tw/edu/ncu/cc/location/server/service/impl/WordServiceImplTest.groovy
+++ location-server/src/test/groovy/tw/edu/ncu/cc/location/server/service/impl/WordServiceImplTest.groovy
@@ -2,8 +2,8 @@ package tw.edu.ncu.cc.location.server.service.impl
 
 import org.springframework.beans.factory.annotation.Autowired
 import specification.SpringSpecification
+import tw.edu.ncu.cc.location.data.keyword.Word
 import tw.edu.ncu.cc.location.data.keyword.WordType
-import tw.edu.ncu.cc.location.server.helper.data.LuceneWord
 import tw.edu.ncu.cc.location.server.service.WordService
 
 class WordServiceImplTest extends SpringSpecification {
@@ -17,20 +17,30 @@ class WordServiceImplTest extends SpringSpecification {
 
     def "it can index the given word"() {
         given:
-            def word = new LuceneWord()
+            def word = new Word()
             word.setIndex( "中央研究院-核心部門" )
-            word.setWord( "中央研究院" )
-            word.setType( WordType.UNIT )
+            word.setWord ( "中央研究院" )
+            word.setType ( WordType.UNIT )
         when:
             wordService.persistWords( word )
+        and:
+            def words = wordService.getWords( "中央" )
         then:
-            wordService.getWords( "中央" ).size() != 0
+            words.size() != 0
+        and:
+            words[0].index == "中央研究院-核心部門"
+            words[0].word  == "中央研究院"
+            words[0].type  == WordType.UNIT
     }
 
     def "it can clear all indexed words"() {
         given:
             wordService.persistWords(
-                    new LuceneWord( "關鍵字", "實際值", WordType.UNIT)
+                    new Word(
+                            index: "關鍵字",
+                            word:"實際值",
+                            type:WordType.UNIT
+                    )
             )
         when:
             wordService.clearAllWords()
