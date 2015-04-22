893d898dcbceee2c0d03561a66ed16f87a2c1925
jasonChiu <jason1122g@gmail.com>
2015-04-22 10:11:19 +0800

    refactor location data

---
+++ location-data/src/main/groovy/tw/edu/ncu/cc/location/data/keyword/Word.groovy
@@ -0,0 +1,8 @@
+package tw.edu.ncu.cc.location.data.keyword;
+
+public class Word {
+
+    def String word;
+    def WordType type;
+
+}
---
+++ location-data/src/main/groovy/tw/edu/ncu/cc/location/data/location/Location.groovy
@@ -0,0 +1,8 @@
+package tw.edu.ncu.cc.location.data.location;
+
+public class Location {
+
+    def double lat;
+    def double lng;
+
+}
