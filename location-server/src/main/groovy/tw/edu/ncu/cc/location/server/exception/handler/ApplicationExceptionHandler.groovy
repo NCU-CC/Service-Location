3ecced8d8a44751f05ad4eb62c5fe59759e848e7
jasonChiu <jason1122g@gmail.com>
2015-07-21 09:57:57 +0800

    migrate exception handler

--- location-server/src/main/groovy/tw/edu/ncu/cc/location/server/exception/handler/ApplicationExceptionHandler.groovy
+++ location-server/src/main/groovy/tw/edu/ncu/cc/location/server/exception/handler/ApplicationExceptionHandler.groovy
@@ -5,19 +5,51 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.TypeMismatchException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.client.HttpServerErrorException
+import tw.edu.ncu.cc.oauth.data.v1.message.ErrorObject
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
     private Logger logger = LoggerFactory.getLogger( this.getClass() )
 
-    @ExceptionHandler( MissingServletRequestParameterException )
-    public ResponseEntity missingParameter( MissingServletRequestParameterException e ) {
+    @ExceptionHandler( HttpServerErrorException )
+    def ResponseEntity httpServerError( HttpServerErrorException e ) {
         new ResponseEntity<>(
-                "missing parameters:" + e.parameterName, HttpStatus.BAD_REQUEST
+                new ErrorObject( e.message ), e.statusCode
+        )
+    }
+
+    @ExceptionHandler( MissingServletRequestParameterException )
+    def ResponseEntity invalidParams( MissingServletRequestParameterException e ) {
+        new ResponseEntity<>(
+                new ErrorObject(
+                        "missing parameter:" + e.parameterName
+                ), HttpStatus.BAD_REQUEST
+        )
+    }
+
+    @ExceptionHandler( [ HttpMessageNotReadableException, HttpMediaTypeNotSupportedException ] )
+    def ResponseEntity invalidRequestBodyFormat() {
+        new ResponseEntity<>(
+                new ErrorObject(
+                        "expect request in json format"
+                ), HttpStatus.BAD_REQUEST
+        )
+    }
+
+    @ExceptionHandler( HttpRequestMethodNotSupportedException )
+    def ResponseEntity invalidMethod( HttpRequestMethodNotSupportedException e ) {
+        new ResponseEntity<>(
+                new ErrorObject(
+                        ( "method not supported:" + e.method + ", expect:" + Arrays.toString( e.getSupportedMethods() ) ) as String
+                ), HttpStatus.METHOD_NOT_ALLOWED
         )
     }
 
@@ -32,7 +64,9 @@ public class ApplicationExceptionHandler {
     public ResponseEntity unhandledException( Exception e ) {
         logger.error( "UNEXPECTED ERROR", e )
         new ResponseEntity<>(
-                e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR
+                new ErrorObject(
+                        e.message
+                ), HttpStatus.INTERNAL_SERVER_ERROR
         )
     }
 
